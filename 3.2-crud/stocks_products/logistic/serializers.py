6578e7ec9ed88a20319957efeb81f63d15224364
Artem <karimov.art89@gmail.com>
2024-08-11 18:29:15 +0300

    create and update methods rework, add solution for additional exercise

--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -20,24 +20,21 @@ class StockSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Stock
-        fields = "__all__"
+        fields = ('id', 'address', 'positions',)
 
     def create(self, validated_data):
         # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
         # создаем склад по его параметрам
         stock = super().create(validated_data)
-        # здесь вам надо заполнить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
         for position in positions:
-            # StockProduct.objects.create(stock=stock, **position)
-            product = Product.objects.filter(id=position.get('product'))
             StockProduct.objects.get_or_create(
                 stock=stock,
-                product=product,
-                quantity=position.get('quantity'),
-                price=position.get('price'),
+                product=position.get('product'),
+                defaults={
+                    'quantity': position.get('quantity'),
+                    'price': position.get('price')
+                },
             )
         return stock
 
@@ -47,15 +44,13 @@ class StockSerializer(serializers.ModelSerializer):
         print(positions)
         # обновляем склад по его параметрам
         stock = super().update(instance, validated_data)
-        # здесь вам надо обновить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
         for position in positions:
-            product = Stock.objects.filter(id=position.get('product'))
             StockProduct.objects.update_or_create(
                 stock=stock,
-                product=product,
-                quantity=position.get('quantity'),
-                price=position.get('price'),
+                product=position.get('product'),
+                defaults={
+                    'quantity': position.get('quantity'),
+                    'price': position.get('price'),
+                },
             )
         return stock
--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -1,4 +1,4 @@
-from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.filters import SearchFilter
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
@@ -10,13 +10,14 @@ from logistic.serializers import ProductSerializer, StockSerializer
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    # при необходимости добавьте параметры фильтрации
-    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filter_backends = [SearchFilter, ]
+    search_fields = ['title', 'description', ]
 
 
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
-    # при необходимости добавьте параметры фильтрации
-    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
-    # pagination_class = LimitOffsetPagination
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['products', ]
+    search_fields = ['products__title', 'products__description', ]
+    pagination_class = LimitOffsetPagination
